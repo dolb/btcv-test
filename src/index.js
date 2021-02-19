1a77ca6f9ded388b24c293fb3d2936ac19f1d511
dolb <dolb1987@gmail.com>
2021-02-19 17:12:15 +0100

    Trying to sign P2PKH or P2SH-P2WPKH packs.

--- src/index.js
+++ src/index.js
@@ -1,70 +1,92 @@
-import express from 'express'
-import bodyParser from 'body-parser'
+import TransactionHelper from './transactionHelper'
 import axios from 'axios'
 
-const app = express()
-const port = 3000
-app.listen(port, () => {
-  console.log(`Server started at http://localhost:${port}`)
-})
-
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-
-app.get('/', (req, res) => {
-  res.send('PONG')
-})
-
-function getInputData(
-  unspent,
-) {
-  return {
-    hash: unspent.id,
-    index: unspent.idx,
-    witnessUtxo: {
-      value: unspent.balance,
-      script: Buffer.from(unspent.scriptHex, 'hex'),
+const config = {
+    jsonRpc: {
+        url: 'http://127.0.0.1:18332',
+        headers: {
+            'Authorization': 'Basic dXNlcjpwYXNz',
+            'content-type': 'application/json'
+        } 
     }
-  };
+}
+const xprv = 'xprv9s21ZrQH143K2tHFe1yiD7vC4EtQwqETSc3chCePr5jqYZXnBsWznpmF2skdNsZ94XdCNnT6UBQMhuwXUo8771xQsC91oVxWr54D4jT5LKE'
+
+const transactionHelper = new TransactionHelper(config, xprv)
+
+const withdraw = {
+    data: {
+        operationId: 'whateveraaa',
+        outputs: [
+            {
+                amount: 16000,
+                address: 'YdbL2qBqVAx8xZLbfGkbCg8v6a3jTWyZB8'
+            }
+        ],
+        unspents: [
+            {
+                amount: 20000,
+                id: 'b4260448d690de21ecc8f9a2cda0e6adc1aaf03eb61ad4b5f84f899add3189af',
+                idx: 1,
+                scriptHex: '76a9149c14b786c1174dba64bb758a5c2e6eac77d7f93588ac',
+                addressBase: '00000000',
+                rawTransactionHex: '02000000000101efdd3aa00415bb9fe55b4e3aefd2699fd22a35c22f9314e22a7380ae312d81060100000000fdffffff02204e0000000000001976a9149c14b786c1174dba64bb758a5c2e6eac77d7f93588ac3dc7040000000000160014b79adbb4706db431605259c713e83cbec67ecd8002483045022100e4c1a322d9af06a46aaee746c37e0d705e9535c8175815ad94638941acf2404702201cc98ff9fd180bf890aaabc2b584c1e6e8c3b4d434e59fdbc7065702b0d284ae012102b13c44b27d23cf64e8f215e25111928a27ac1678e01bba66eb1dd6c80c73c91ad8180100',
+                isScript: false,
+            }
+        ]
+    }
 }
 
-const btcvjs = require('./btcvjs-lib')
-const sendRestTo = 'royale1qfqqau0hw2dnps5kdx0lf6kuyg60wyl6uy0yfme'
 
-const processTest = async (req, res) => {
-  const { data, sendTo, amount } = req.body
-  const netParams = btcvjs.alt_networks.bitcoinvault
-  try {
-    var txb = new btcvjs.Psbt({network: netParams});
-    const { totalBalance, fee, unspents } = data;
-    const whatIsLeft = totalBalance - fee - amount;
-    unspents.forEach(el => {
-      const input = getInputData(el)
-      console.log(input);
-      txb.addInput(input);
-    })
-    txb.addOutput({address: sendTo, value: amount});
-    txb.addOutput({address: sendRestTo, value: whatIsLeft});
-    console.log(txb);
-    unspents.forEach((el, i) => {
-      txb.signInput(i, btcvjs.ECPair.fromWIF(el.wif , netParams));
-    })
-
-    txb.finalizeAllInputs();
-  
-    const body = txb.extractTransaction().toHex()
-    res.json({result: 'ok', body });
-  } catch(e) {
-    console.error(e);
-    res.status(502).json(
-      {
-        code: 9000,
-        message: e
-      }
+const publishTransaction = async (withdraw) => {
+    const { operationId, body } = withdraw
+    const {jsonRpc} = config
+    const {url, headers} = jsonRpc
+    const rpcCall = await axios.post(
+        url,
+        {
+            jsonrpc: '1.0',
+            id: (new Date()).getTime().toString(), 
+            method: 'sendrawtransaction', 
+            params: [
+                body.toString()
+            ]
+        },
+        {headers}
     )
-  }
-  
+    const { transactionId } = rpcCall
+    if(rpcCall.hex != null) {
+        return {
+            result: 'ok',
+            operationId,
+            transactionId
+        }
+    }
+
+    return {
+        result: 'fail'
+    }
 }
 
+const test = async (h) => {
+    //const address = await transactionHelper.generateP2SH('00000000')
+    //console.log(address.redeem.output.toString('hex'));
+    const p2pkh = await h.generateP2PKH('00000000')
+    console.log(p2pkh.address);
+    const signedTransaction = await h.sign(withdraw)
+    console.log(signedTransaction);
+    /*try {
+        const res = await publishTransaction(signedTransaction)
+        console.log(res);
+        console.log('AAAAAAAAAAA');
+    }catch(e) {
+        console.error(e.response);
+        console.log('ZZZZZZZ');
+    }*/
+    
+}
+//const signedTransaction = transactionHelper.sign(withdraw, true)
+test(transactionHelper)
 
-app.post('/test', processTest)
+
+//console.log(signedTransaction);
---
+++ src/transactionHelper.js
@@ -0,0 +1,152 @@
+import * as R from 'ramda'
+import * as bip32 from 'bip32'
+import * as lib from 'bitcoinjs-lib'
+
+const netParams = {
+    messagePrefix: '\x18Bitcoin Signed Message:\n',
+    bech32: 'royale',
+    bip32: {
+      public: 0x0488b21e,
+      private: 0x0488ade4,
+    },
+    pubKeyHash: 0x4e,
+    scriptHash: 0x3c,
+    wif: 0x80,
+  };
+
+
+
+export default class TransactionHelper {
+    constructor(config, xprv) {
+        this._config = config;
+        this._xprv = xprv;
+        this._mainNode = bip32.fromBase58(this._xprv, netParams)
+        console.log('Initialized main transaction');
+    }
+
+    prepareNonWitnessUtxo = (unspent) => {
+        return Buffer.from(`${unspent.rawTransactionHex}`, 'hex')
+    }
+
+    getInputData = async (unspent) => {
+        const isScript = !!unspent.isScript
+
+        if(isScript) {
+            const p2sh = await this.generateP2SH(unspent.addressBase)
+            return {
+                hash: unspent.id,
+                index: unspent.idx,
+                witnessUtxo: {
+                    value: unspent.amount,
+                    script: Buffer.from(unspent.scriptHex, 'hex'),
+                },
+                redeemScript: p2sh.redeem.output,
+            };
+        }
+
+        if(unspent.rawTransactionHex) {
+            return {
+                hash: unspent.id,
+                index: unspent.idx,
+                nonWitnessUtxo: this.prepareNonWitnessUtxo(unspent)
+            }    
+        }
+    
+        return {
+            hash: unspent.id,
+            index: unspent.idx,
+            witnessUtxo: {
+                value: unspent.amount,
+                script: Buffer.from(unspent.scriptHex, 'hex'),
+            }
+        };
+    }
+
+    nodeToAddress(hdNode) {
+        const { address } = lib.payments.p2wpkh({ pubkey: hdNode.publicKey, network: netParams })
+        return address;
+    }
+
+    generateP2SH = async (hexString) => {
+        const path = this.preparePath(hexString)
+        const node = R.reduce((derrived, p) => derrived = derrived.derive(p), bip32.fromBase58(this._xprv), path)
+        const p2wpkh = lib.payments.p2wpkh({ pubkey: node.publicKey, network: netParams })
+        return lib.payments.p2sh(
+            {
+                redeem: p2wpkh
+            }
+        )
+    }
+
+    generateP2PKH = async (hexString) => {
+        const path = this.preparePath(hexString)
+        const node = R.reduce((derrived, p) => derrived = derrived.derive(p), bip32.fromBase58(this._xprv), path)
+        return lib.payments.p2pkh({ pubkey: node.publicKey, network: netParams })
+    }
+
+
+    generateAddress = async (hexString) => {
+        const path = this.preparePath(hexString)
+        const node = R.reduce((derrived, p) => derrived = derrived.derive(p), bip32.fromBase58(this._xprv), path)
+        return this.nodeToAddress(node)
+    }
+
+    preparePath(input) {
+        return `${input.padStart(input.length + input.length % 8, '0')}0`
+        .match(/.{1,8}/g)
+        .map(el => parseInt(el, 16) % 0x80000000)
+    }
+
+    deriveWif = (addressBase) => {
+        const path = this.preparePath(addressBase)
+        const node = R.reduce((derrived, p) => derrived = derrived.derive(p), bip32.fromBase58(this._xprv), path)
+        const derrivedWif = node.toWIF()
+        return derrivedWif
+    }
+
+    sign = async (withdrawData) => {
+        const { data } = withdrawData
+
+        try {
+            var txb = new lib.Psbt({network: netParams});
+            const { unspents, outputs, operationId } = data;
+
+            for(var i=0; i<unspents.length; i++) {
+                const el = unspents[i]
+                const input = await this.getInputData(el)
+                console.log(input);
+                txb.addInput(input)
+            }
+
+            outputs.forEach(el => {
+                const { address, amount, fee } = el
+                txb.addOutput({address, value: (amount - (fee || 0))});
+            })
+
+            unspents.forEach((el, i) => {
+                const derivedWif = this.deriveWif(el.addressBase)
+                txb.signInput(i, lib.ECPair.fromWIF(derivedWif , netParams));
+            })
+
+            txb.validateSignaturesOfAllInputs() 
+
+            txb.finalizeAllInputs();
+
+            const transaction = txb.extractTransaction()
+            const body = transaction.toHex()
+            const transactionId = transaction.getId()
+            console.log(`Signed ${transactionId}`);
+            return {result: 'ok', body, operationId, transactionId };
+        } catch(e) {
+            console.error(e);
+            return {
+                result: 'fail',
+                code: 9000,
+                message: e
+            }
+        }
+    }
+
+
+
+}
